638a118c7ca83833372f04d2bff9c2156935d31e
mcdr65 <andre_meichtry@bluewin.ch>
2025-01-30 15:53:47 +0100

    second

--- app.R
+++ app.R
@@ -15,8 +15,8 @@ ll_MLE <- log_likelihood(theta_MLE, theta_MLE, sigma)
 
 
 ui <- fluidPage(
-    titlePanel("Trinity"),
-    verticalLayout(sliderInput(inputId="theta_0",label="Null Hypothesis",min=-2,max=2,value=0,step=0.1),
+    titlePanel("One-parameter model: Trinity of asymptotic tests"),
+    verticalLayout(sliderInput(inputId="theta_0",label="Null Hypothesis (Constraint)",min=-2,max=2,value=0,step=0.1),
                     plotOutput(outputId="LL",width="50%")))
 
 
