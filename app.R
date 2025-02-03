365c5f66a6018c5563674905013da20f1c8164e8
mcdr65 <andre_meichtry@bluewin.ch>
2025-02-03 16:20:41 +0100

    changeApp

--- app.R
+++ app.R
@@ -1,49 +1,60 @@
 library(shiny)
 library(ggplot2)
-# Define the log-likelihood function
-log_likelihood <- function(MLE,theta, sigma) {
-  -0.5 * ((MLE - theta)^2 / sigma^2)
-}
-# Parameters
-theta_MLE <- 0
-sigma <- 1
-theta_seq <- seq(-3, 3, length.out = 100)
-# Calculate log-likelihood values
-ll_values <- log_likelihood(theta_seq, theta_MLE, sigma)
-ll_MLE <- log_likelihood(theta_MLE, theta_MLE, sigma)
 
+ll<-function(theta,x,n) {
+    dbinom(x=x,size=n,prob=theta,log=TRUE)-dbinom(x=x,size=n,prob=x/n,log=TRUE)
+}
+
+qa<-function(theta,mle,se) {
+    -0.5 * ((mle - theta)^2 / se^2)
+}
 
 
 ui <- fluidPage(
     titlePanel("One-parameter model: Trinity of asymptotic tests"),
-    verticalLayout(sliderInput(inputId="theta_0",label="Null Hypothesis (Constraint)",min=-2,max=2,value=0,step=0.1),
-                    plotOutput(outputId="LL",width="50%")))
+    sliderInput(inputId="x",label="Data: Counts out of 100",min=0,max=100,value=50,step=1),
+    sliderInput(inputId="theta_0",label="Null Hypothesis (Constraint)",min=0.1,max=0.9,value=0.5,step=0.01),   
+    plotOutput(outputId="LL",width="50%")
+)
 
 
 server <- function(input, output) {
-    output$LL<-renderPlot( {
-        ll_null <- log_likelihood(input$theta_0, theta_MLE, sigma)
-        slope <- (theta_MLE - input$theta_0) / sigma^2
-        tangent_range <- seq(input$theta_0 - 0.5, input$theta_0 + 0.5, length.out = 100)
+    output$LL<-renderPlot({
+        n<-100
+        mle<-input$x/n
+        se<-sqrt(mle*(1-mle)/n)
+        theta_seq<-seq(0.05,0.95,.01)
+        # Calculate log-likelihood values
+        ll_values <- ll(theta_seq,input$x,n)
+        ll_MLE <- ll(mle,input$x,n)
+        qa_values <- qa(theta_seq,mle=mle,se=se)
+        qa_MLE <- qa(mle,mle=mle,se=se)
+        ll_null <- ll(input$theta_0,input$x,n)
+        qa_null <- qa(input$theta_0,mle=mle,se=se)
+        slope <- input$x/input$theta_0-(n-input$x)/(1-input$theta_0)
+        tangent_range <- seq(input$theta_0 - 0.1, input$theta_0 + 0.1, length.out = 100)
         tangent_line <- ll_null + slope * (tangent_range - input$theta_0)
-        data <- data.frame(theta = theta_seq,ll = ll_values)
+        data   <- data.frame(theta = theta_seq,ll = ll_values)
+        dataqa <- data.frame(theta = theta_seq,qa = qa_values)
         tangent_data <- data.frame(theta = tangent_range,ll = tangent_line)
-        ggplot(data, aes(x = theta, y = ll)) +
+        ggplot(data, aes(x = theta, y = ll))+ ylim(-100,10)+
             geom_line() +
-            geom_vline(xintercept = theta_MLE, linetype = "dashed", color = "blue") +
-            geom_vline(xintercept = input$theta_0, linetype = "dashed", color = "red") +
-            geom_hline(yintercept = 0, linetype = "dotted",color="black") +
-            geom_hline(yintercept = ll_null, linetype = "dotted",color="black") +
-            geom_segment(aes(x = theta_MLE, y = -3, xend = input$theta_0, yend = -3),
-                         arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "cm")), color = "blue",lwd=1.5) +
-            geom_segment(data = tangent_data, aes(x = theta, y = ll, xend = theta + 0.02, yend = ll + 0.01 * slope),  lwd=1.5,color = "red") +
-            geom_segment(aes(x = input$theta_0+.5, y = ll_null, xend = input$theta_0+.5, yend = ll_MLE),
-                         arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "cm")), lwd=1.5,color = "black") +
-            labs(x = expression(theta),y = "Log-Likelihood") +
-            scale_x_continuous(breaks = c(input$theta_0, theta_MLE), labels = c(expression(theta[0]), expression(hat(theta)[MLE]))) +
-            annotate("text", x = theta_MLE+1, y = -3, label = expression(Wald~Test), color = "blue", vjust = -1, hjust = 1.5) +
-            annotate("text", x = input$theta_0+0.1, y = ll_null-0.5, label = expression(Score~Test), color = "red", vjust = -1, hjust = -0.5) +
-            annotate("text", x = input$theta_0+.5, y = (ll_null + ll_MLE) / 2, label = expression(LR~Test), color = "black", vjust = -1, hjust = -0.5)
+            geom_vline(xintercept = mle, linetype = "dashed", color = "blue") +
+            geom_vline(xintercept = input$theta_0, linetype = "dashed", color = "brown") +
+            geom_hline(yintercept = 0, linetype = "dotted",color="brown") +
+            geom_hline(yintercept = ll_null, linetype = "dotted",color="brown") +
+            geom_segment(aes(x = mle, y = qa_null, xend = input$theta_0, yend = qa_null),
+                         arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "cm")), linetype="dashed",color = "blue",lwd=1.5) +
+            geom_segment(data = tangent_data, aes(x = theta[1], y = ll[1], xend = theta[100], yend = ll[100]),  lwd=1.5,color = "red") +
+            geom_segment(aes(x = input$theta_0+.2, y = ll_null, xend = input$theta_0+.2, yend = ll_MLE),
+                         arrow = arrow(type = "closed", ends = "both", length = unit(0.2, "cm")), lwd=1.5,color = "brown") +
+            labs(x = expression(theta),y = "Log-Likelihood (black), quadratic approximation (dashed)") +
+            scale_x_continuous() +
+             ## scale_x_continuous(breaks = c(input$theta_0, mle), labels = c(expression(theta[0]), expression(hat(theta)[MLE])))+ 
+            annotate("text", x = input$theta_0, y = qa_null-0.5, label = expression(Wald),color = "blue", vjust = -1, hjust = 1.5) +
+            annotate("text", x = (input$theta_0+mle)/2, y = ll_null, label = expression(Score), color = "red", vjust = -1, hjust = -0.5)+
+            annotate("text", x = input$theta_0+0.1, y = (ll_null + ll_MLE) / 2, label = expression(LR), color = "brown", vjust = -1, hjust = -0.5)+
+            geom_line(data=dataqa,aes(x=theta,y=qa),linetype="dashed")
     })
 }    
 
